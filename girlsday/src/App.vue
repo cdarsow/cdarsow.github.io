0c6f650044f0ebf80712a8e19501697b9bb5c626
Christian Darsow <hello@darsow.io>
2025-03-28 14:24:33 +0100

    Add Monster

--- girlsday/src/App.vue
+++ girlsday/src/App.vue
@@ -10,6 +10,7 @@
 <script setup lang="ts">
 import { useTemplateRef, onMounted } from "vue";
 import rainbow from "@/images/rainbow.png";
+import monster from "@/images/monster.png";
 import coolFont from "@/fonts/Knewave-Regular.ttf";
 
 const canvas = useTemplateRef<HTMLCanvasElement>("canvas");
@@ -22,48 +23,6 @@ function theatorFit() {
     return;
   }
 
-  /*
-  calculate dimensions for if the width was the screen width, and the height was still proportionate. This will be used in a later calculation.
-
-  formula for getting new height using new width and aspect ratio:
-  aspectRatio = oldHeight/oldWidth;
-  newHeight = aspectRatio*newWidth;
-  */
-  const dims = {
-    width: window.innerWidth,
-    height: (canvasDims.height / canvasDims.width) * window.innerWidth,
-  };
-  console.log(dims);
-  /* set aspect ratio in CSS. This ensures that the element is always a certain aspect ratio, even if width or height isn't given.
-
-  aspect ratio is given by the formula: width/height = aspectRatio;
-  */
-  // console.log(canvas.value.style.aspectRatio);
-
-  // canvas.value.style.aspectRatio = (
-  //   canvasDims.width / canvasDims.height
-  // ).toString();
-
-  // console.log(canvas.value.style.aspectRatio);
-
-  /*
-  use "dims" to determine proper fit.
-
-  If the screen height is more than the new canvas height when the width of the canvas is maximized, that means theres vertical space, and none of the height will be cut off, so we maximize the width, and set height to initial as to allow it to naturally conform to the aspect ratio (incase it was previously set to 100%).
-
-Otherwise, if the new canvas height is more than the screen height, that means some of the canvas height will be cut off by the viewport, therefore we should maximize the height instead, and set the width to initial.
-  */
-  // if (window.innerHeight > dims.height) {
-  //   console.log("ja");
-  //   canvas.value.style.width = "100%";
-  //   canvas.value.style.height = "initial";
-  //   canvas.value.style.top = `calc(50% - ${dims.height + "px"}/2)`;
-  // } else {
-  //   canvas.value.style.height = "100%";
-  //   canvas.value.style.width = "initial";
-  //   canvas.value.style.top = "initial";
-  // }
-
   canvas.value.style.height = window.innerHeight + "px";
   canvas.value.style.width =
     (window.innerHeight * canvasDims.width) / canvasDims.height + "px";
@@ -72,7 +31,6 @@ Otherwise, if the new canvas height is more than the screen height, that means s
     canvasDims.height /
     window.innerHeight
   ).toString();
-  // canvas.value.style.height = window.innerHeight + "px";
 }
 
 onMounted(() => {
@@ -87,6 +45,7 @@ onMounted(() => {
     if (!context || !canvas.value) {
       return;
     }
+
     context.font = "48px myCoolFont";
     context.lineWidth = 5;
 
@@ -117,33 +76,52 @@ onMounted(() => {
   canvas.value.addEventListener("mousedown", someInfo);
 
   let y = 0;
-  let imageLoaded = false;
+  let x = 50;
+  let stepCounter = 0;
+  const realSpeed = 5;
+  let speed = realSpeed;
+  let imageLoaded1 = false;
+  let imageLoaded2 = false;
 
-  function square() {
+  function drawMonster() {
     if (!context) {
       return;
     }
+    if (!imageLoaded2) {
+      return;
+    }
 
-    y += 1;
-    context.beginPath();
-    context.rect(25, y, 100, 100);
-    context.lineWidth = 5;
-    context.stroke();
-    context.fillStyle = "teal";
-    context.fill();
+    stepCounter++;
+    if (stepCounter > 200 / realSpeed) {
+      if (speed > 0) {
+        speed = -1 * realSpeed;
+      } else {
+        speed = realSpeed;
+      }
+      stepCounter = 0;
+    }
+
+    x += speed;
+    context.drawImage(monsterImage, x, 500);
   }
 
   const rainbowImage = new Image();
   rainbowImage.src = rainbow;
   rainbowImage.addEventListener("load", () => {
-    imageLoaded = true;
+    imageLoaded1 = true;
+  });
+
+  const monsterImage = new Image();
+  monsterImage.src = monster;
+  monsterImage.addEventListener("load", () => {
+    imageLoaded2 = true;
   });
 
   function renderBackground() {
     if (!context) {
       return;
     }
-    if (imageLoaded) {
+    if (imageLoaded1) {
       context.drawImage(rainbowImage, 0, 200);
     }
   }
@@ -154,10 +132,12 @@ onMounted(() => {
     }
 
     // erase entire canvas
-    // context.clearRect(0, 0, canvas.value.width, canvas.value.height);
+    context.clearRect(0, 0, canvas.value.width, canvas.value.height);
 
     // render each type of entity in order, relative to layers
     renderBackground();
+    drawMonster();
+    addText();
     // square();
     // renderProps();
     // renderCharacters();
